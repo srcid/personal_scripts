b8007bf4824fe8e7cd12c24716a01e74525a12d7
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2020-10-31 22:13:16 -0300

    script that download albums

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # personal_scripts
+
+This repository is only to backup my personal scripts
+
+What does each of them do?
+
+album-dl.sh --> downlaod playlists of albums
---
+++ album-dl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+
+youtube-dlc \
+	--verbose \
+	--format bestaudio \
+	--extract-audio \
+	--audio-quality 0 \
+	--audio-format "m4a" \
+	--yes-playlist \
+	--embed-thumbnail \
+	--add-metadata \
+	--output "~/Music/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+	$1
+
+
+
